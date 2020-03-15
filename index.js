0ebd15261e55015423a9fc3d190a9dac9898934c
busradanisman <bdanisman18@posta.pau.edu.tr>
2020-03-15 17:17:01 +0300

    update

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+let db = [];
+
+app.get('/', (req, res) => {
+    res.json(db)
+})
+
+app.get('/add/:number', (req, res) => {
+    console.log(req.params.number)
+    db.push(Number(req.params.number))
+    res.json(db)
+})
+
+app.get('/delete/:number', (req, res) => {
+    db = db.filter(item => item !== +req.params.number)
+    res.json(db)
+})
+
+app.get('/update/:target/:destination', (req, res) => {
+    db = db.map((row) => {
+      if (row == +req.params.target) {
+        row = +req.params.destination
+      }
+      return row
+    })
+  res.json(db)
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
